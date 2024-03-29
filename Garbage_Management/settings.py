50bbc9ef3e3b85cb6650db6dc5630a2107bd4dcf
Nandhukriss <103727372+Nandhukriss@users.noreply.github.com>
2024-03-29 12:31:27 +0530

    🐳 docker file added

--- Garbage_Management/settings.py
+++ Garbage_Management/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-1kxfz+p4cx+zpti)*jg-(g@x^x0ij&gk3ol6$)%2t&m!m(#+h(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
